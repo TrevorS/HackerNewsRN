3dfde1e8d76f038165e844ac6dcbfd34d240c80a
Trevor Strieber <trevor@strieber.org>
2017-02-20 18:14:41 -0800

    Simplify screens.

--- js/screens/CommentsScreen.js
+++ js/screens/CommentsScreen.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { View, Button } from 'react-native';
-import { NavigationActions } from 'react-navigation';
 
-const CommentsScreen = ({ navigation }) => (
-  <View>
-    <Button
-      title="CommentsScreen"
-      onPress={() => navigation.dispatch(NavigationActions.navigate({ routeName: 'Stories' }))}
-    />
-  </View>
+import { CommentListContainer } from '../containers';
+
+const CommentsScreen = () => (
+  <CommentListContainer />
 );
-
-CommentsScreen.propTypes = {
-  navigation: React.PropTypes.shape({
-    navigate: React.PropTypes.func.isRequired,
-    dispatch: React.PropTypes.func.isRequired,
-  }).isRequired,
-};
 
 CommentsScreen.navigationOptions = {
   title: 'Comments',
--- js/screens/StoriesScreen.js
+++ js/screens/StoriesScreen.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
 
 import { StoryListContainer } from '../containers';
 
 const StoriesScreen = () => (
-  <View>
-    <StoryListContainer />
-  </View>
+  <StoryListContainer />
 );
-
-StoriesScreen.propTypes = {
-  navigation: React.PropTypes.shape({
-    navigate: React.PropTypes.func.isRequired,
-    dispatch: React.PropTypes.func.isRequired,
-  }).isRequired,
-};
 
 StoriesScreen.navigationOptions = {
   title: 'HackerNewsRN',
